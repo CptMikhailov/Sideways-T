f7e1a6926d410dee475ae63a93a3ec4aa3106387
David Host <hostdm@outlook.com>
2018-07-19 02:48:33 -0400

    Removed unused scripts

--- SidewaysT/wwwroot/js/site.js
+++ SidewaysT/wwwroot/js/site.js
@@ -2,7 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
-
-function EnableWepSelect() {
-    document.getElementById("wepSelect").removeAttribute("disabled");
-}
